253dd25840e238508a3475d858dc951017505bfd
Tom Hsu <thomaschsu@gmail.com>
2019-04-07 20:46:50 -0400

    Added js and styled some more

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// Function to make navbar change color
+$(function() {
+    $(document).scroll(function() {
+        var $nav = $('#navbar');
+        $nav.toggleClass('scroll', $(this).scrollTop() > $nav.height());
+    });
+});
